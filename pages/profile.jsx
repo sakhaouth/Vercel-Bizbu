c1e4356c69230d9d486b21343cbfbe8ae0894ba6
sakhaouth <nsakhawathhossan29@gmail.com>
2023-04-11 14:42:18 +0600

    dekha jak

--- pages/profile.jsx
+++ pages/profile.jsx
@@ -1,35 +1,11 @@
-function UserProfile({data})
+function UserProfile()
 {
-    console.log(data)
+    
     return(
         <>
-            <div>{data.squadName}</div>
+            <div>Hello</div>
         </>
     )
 }
-// export async function getServerSideProps() {
-//     // Fetch data from external API
-//     const res = await fetch('http://localhost:3000/api/hello')
-//     const data = await res.json()
-//     console.log(data)
-//     // Pass data to the page via props
-//     return { props: { data }
 
-
-//     }
-//   }
-  export async function getStaticProps() {
-    // const res = await fetch('http://localhost:3000/api/hello')
-    // const data = await res.json()
-  
-    // return {
-    //   props: {
-    //     data,
-    //   },
-    //   // Next.js will attempt to re-generate the page:
-    //   // - When a request comes in
-    //   // - At most once every 10 seconds
-    //   revalidate: 10, // In seconds
-    // }
-  }
 export default UserProfile
